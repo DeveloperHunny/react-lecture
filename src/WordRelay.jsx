235cb7c415f27b8cf6fe73bc20f215649d3218c9
DeveloperHunny <eogns0321@gmail.com>
2022-05-16 09:31:10 +0900

    [add] class WordRelay Complete & Hot server

--- src/WordRelay.jsx
+++ src/WordRelay.jsx
@@ -3,14 +3,54 @@ const React = require('react');
 class WordRelay extends React.Component{
 
     state = {
-        text : "Hello, webpack",
+        word : '끝말잇기',
+        value : 'EMPTY',
+        result : 'EMPTY',
     };
+
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        console.log(this.state.word[-1]);
+        if(this.state.word[this.state.word.length - 1] === this.state.value[0]){
+            this.setState({
+                result : '딩동댕',
+                word : this.state.value,
+                value : '',
+            });
+            this.inputRef.focus();
+        }   
+        else{
+            this.setState({
+                result : '땡',
+                value : '',
+            })
+        }
+        
+    }
+
+    onChangeInput = (e) => {
+        this.setState({
+            value : e.target.value,
+        })
+    }
+
+    inputRef;
+    onRefInput = (c) => {
+        this.inputRef = c;
+    }
+
+
 
     render(){
         return(
-            <div>
-                <h1>{this.state.text}</h1>
-            </div>
+            <>
+                <div>{this.state.word}</div>
+                <form onSubmit={this.onSubmitForm}>
+                    <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput}/>
+                    <button>입력!</button>
+                </form>
+                <div>{this.state.result}</div>
+            </>
         );
     }
 
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
     name : 'word-relay-setting',
@@ -13,17 +15,36 @@ module.exports = {
 
     module : {
         rules : [{
-            test : /\.jsx?/,
+            test : /\.jsx?$/,
             loader : 'babel-loader',
             options : {
-                presets :['@babel/preset-env' , '@babel/preset-react'],
-                plugins :['@babel/plugin-proposal-class-properties'],
+                presets :[
+                    ['@babel/preset-env',{
+                        targets : { 
+                            browsers:['> 5% in KR']
+                        },
+                    }], 
+                '@babel/preset-react'],
+                plugins :[
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel'
+                ],
             }
         }],
     },
-
+    plugins : [
+        new webpack.LoaderOptionsPlugin({debug: true}),
+        new RefreshWebpackPlugin
+    ],
     output : { //출력
         path : path.join(__dirname, 'dist'),
-        filename : 'app.js'
+        filename : 'app.js',
+        publicPath : '/dist/'
     },
+    devServer: {
+        devMiddleware: { publicPath: '/dist' },
+        hot: true,
+        static: { directory: path.resolve(__dirname) },
+        
+    }
 };
