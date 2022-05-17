ef37036f55644a937b19324b1188d05ab77c0a30
DeveloperHunny <eogns0321@gmail.com>
2022-05-17 11:52:04 +0900

    [modify] NumberBaseBall

--- src/NumberBaseBall.jsx
+++ src/NumberBaseBall.jsx
@@ -3,11 +3,11 @@ import React, { Component } from "react";
 function getNumbers(){
     let number = '';
     for(let i = 0; i < 4; i++){
-        let num = Math.floor(Math.random() * 10);
+        let num = Math.floor(Math.random() * 9 +1);
         number += num.toString();
     }
 
-    return Number(number);
+    return number;
 }
 
 class NumberBaseBall extends Component{
@@ -20,26 +20,44 @@ class NumberBaseBall extends Component{
             answer : getNumbers(),
             tries : [],
         };
+
+        console.log(this.state.answer);
     }
 
     onSubmitForm = (e) => {
         e.preventDefault();
-        if(this.state.value === this.state.answer){
+        if(Number(this.state.value) === this.state.answer){
             this.setState({
                 result : "정답입니다!",
-                value : ' ',
+                value : '',
             });
         }
         else{
+            // STRIKE BALL 계산
+            let ans = this.state.answer;
+            let inp = this.state.value;
+            let strike = 0;
+            let ball = 0;
+            for(let i = 0; i < 4; i++){
+                if(ans[i] === inp[i]){
+                    strike += 1;
+                }
+                else if (ans.indexOf(inp)){
+                    ball += 1;
+                }
+            }
             this.setState({
-                result : "틀렸습니다!",
-                value : ' ',
+                result : {strike} + 'STRIKE   ' + {ball} + 'BALL',
+                value : '',
                 tires : this.state.tries.concat(this.state.value),
             })
         }
     }
 
-    onChangeInput = (e) => { this.setState({ value : e.target.value })}
+    onChangeInput = (e) => {
+        this.setState({ value : e.target.value })
+        console.log(this.state.value);
+    }
 
     render() {
         return(
@@ -47,11 +65,15 @@ class NumberBaseBall extends Component{
               <h1>{this.state.result}</h1>
               <form onSubmit={this.onSubmitForm}>
                   <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}/>
+                  <button type="submit"> 입력 </button>
               </form>
               <div>시도 횟수 : {this.state.tries.length}</div>
               <ul>
                   {this.state.tries.map((item) => <li> {item} </li>)}
               </ul>
+              <br/>
+
+
           </>
         );
     }
